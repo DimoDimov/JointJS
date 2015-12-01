1422007613fdd7d02f75e192c1c4e212c9cbd688
DimoDimov <dimo.ivanov.dimov@gmail.com>
2015-12-01 11:14:35 +0000

    custom elements added dynamically feature

--- app/app.js
+++ app/app.js
@@ -17,10 +17,120 @@
 
             function controller($scope) {
                 console.log("wrapper Ctrl");
+
+                function declareCustomElements() {
+                    // declare a custom element.
+                    // ------------------------
+
+                    joint.shapes.html = joint.shapes.html || {};
+
+                    joint.shapes.html.Element = joint.shapes.basic.Rect.extend({
+                        defaults: joint.util.deepSupplement({
+                            type: 'html.Element',
+                            attrs: {
+                                rect: {
+                                    stroke: 'none',
+                                    'fill-opacity': 0
+                                }
+                            }
+                        }, joint.shapes.basic.Rect.prototype.defaults)
+                    });
+
+                    // Create a custom view for that element that displays an HTML div above it.
+                    // -------------------------------------------------------------------------
+
+                    joint.shapes.html.ElementView = joint.dia.ElementView.extend({
+
+                        template: [
+                            '<div class="html-element">',
+                            '<button class="delete">x</button>',
+                            '<label></label>',
+                            '<span></span>', '<br/>',
+                            '<select><option>--</option><option>one</option><option>two</option></select>',
+                            '<input type="text" value="I\'m HTML input" />',
+                            '</div>'
+                        ].join(''),
+
+                        initialize: function() {
+                            _.bindAll(this, 'updateBox');
+                            joint.dia.ElementView.prototype.initialize.apply(this, arguments);
+
+                            this.$box = $(_.template(this.template)());
+                            // Prevent paper from handling pointerdown.
+                            this.$box.find('input,select').on('mousedown click', function(evt) {
+                                evt.stopPropagation();
+                            });
+                            // This is an example of reacting on the input change and storing the input data in the cell model.
+                            this.$box.find('input').on('change', _.bind(function(evt) {
+                                this.model.set('input', $(evt.target).val());
+                            }, this));
+                            this.$box.find('select').on('change', _.bind(function(evt) {
+                                this.model.set('select', $(evt.target).val());
+                            }, this));
+                            this.$box.find('select').val(this.model.get('select'));
+                            this.$box.find('.delete').on('click', _.bind(this.model.remove, this.model));
+                            // Update the box position whenever the underlying model changes.
+                            this.model.on('change', this.updateBox, this);
+                            // Remove the box when the model gets removed from the graph.
+                            this.model.on('remove', this.removeBox, this);
+
+                            this.updateBox();
+                        },
+                        render: function() {
+                            joint.dia.ElementView.prototype.render.apply(this, arguments);
+                            this.paper.$el.prepend(this.$box);
+                            this.updateBox();
+                            return this;
+                        },
+                        updateBox: function() {
+                            // Set the position and dimension of the box so that it covers the JointJS element.
+                            var bbox = this.model.getBBox();
+                            // Example of updating the HTML with a data stored in the cell model.
+                            this.$box.find('label').text(this.model.get('label'));
+                            this.$box.find('span').text(this.model.get('select'));
+                            this.$box.css({
+                                width: bbox.width,
+                                height: bbox.height,
+                                left: bbox.x,
+                                top: bbox.y,
+                                transform: 'rotate(' + (this.model.get('angle') || 0) + 'deg)'
+                            });
+                        },
+                        removeBox: function(evt) {
+                            this.$box.remove();
+                        }
+                    });
+                };
+
+                declareCustomElements();
+
+                $scope.addCustomElement = addCustomElement;
+
+                function addCustomElement(x, y, label) {
+                    //creating new Element
+                    var customElement = new joint.shapes.html.Element({
+                        position: {
+                            x: x,
+                            y: y
+                        },
+                        size: {
+                            width: 170,
+                            height: 100
+                        },
+                        label: label,
+                        select: 'one'
+                    });
+
+                    $scope.vm.graph.addCell(customElement);
+
+                    return customElement
+                };
+
+                //-----------------Setting the graph ------------
                 $scope.vm = {};
                 $scope.vm.graph = new joint.dia.Graph;
 
-                //---------Rect------------
+                //---------Rect--------------------------------
                 $scope.addRectangle = addRectangle;
 
                 function addRectangle(x, y, label) {
@@ -48,6 +158,7 @@
                     return rect;
                 };
 
+                //----------------------Circle-----------------
                 $scope.addCircle = addCircle;
 
                 function addCircle(x, y, label) {
@@ -72,6 +183,7 @@
                     return circle;
                 };
 
+                //------------Add Joint-----------------
                 $scope.addJoint = addJoint;
 
                 function addJoint(source, target) {
@@ -99,9 +211,11 @@
 
                 scope.addJoint(rect, rect2);
 
-                var circle = scope.addCircle(200, 230, 'my box');
+                var circle = scope.addCircle(200, 230, 'my circle');
 
                 scope.addJoint(circle, rect);
+
+                scope.addCustomElement(80, 180, 'I\'m custom');
             };
 
             return directive;
@@ -126,7 +240,6 @@
             return directive;
 
             function link(scope, element, attrs) {
-
 
                 var someService = {};
 
